f5692618ddcbf352d0a3fac0d297b6016546a3e2
busukxuan <hobbes.xuan@gmail.com>
2019-03-22 01:01:08 +0800

    Enabled event deletion. Refactored parser, enabled returning `DeleteEventRequest` and `ListEventsRequest`, and added `CalendarHelper.deleteEvent()`.

--- app/src/main/java/edu/sutd/organice/ActionRequest.java
+++ app/src/main/java/edu/sutd/organice/ActionRequest.java
@@ -13,58 +13,80 @@ public abstract class ActionRequest {
     private static final String LOG_TAG = "ActionRequest";
 
     public static ActionRequest parseMessage(TdApi.Message message) throws ParseException {
+        Log.d(LOG_TAG, "parsing message");
         String text = ((TdApi.MessageText) message.content).text.text;
-        Log.d("MyParser", "Called parser.");
+
+        // check each line for command tag
+        String[] lines = text.split("\n");
+        Log.d(LOG_TAG, "n_ines: " + Integer.toString(lines.length));
+        for (int i = 0; i < lines.length; i++) {
+
+            if (lines[i].equals("# organice new")) {
+                EventData eventData = parseEventData(lines, i + 1);
+                return new NewEventRequest(message.chatId, eventData);
+
+            } else if (lines[i].equals("# organice delete")) {
+                EventData eventData = parseEventData(lines, i + 1);
+                return new DeleteEventRequest(message.chatId, eventData);
+
+            } else if (lines[i].equals("# organice list")) {
+                return new ListEventsRequest(message.chatId);
+            }
+        }
+        return null;
+    }
+
+    private static EventData parseEventData(String[] lines, int startLine) throws ParseException {
+        // `EventData` fields are null by default
         String title = null;
-        Date ds = null;// date start
-        Date de = null;// date end
+        Date start = null;
+        Date end = null;
         String venue = null;
         String note = null;
 
-        String[] lines = text.split("\n");
-        Log.d("MyParser", "n_ines: " + Integer.toString(lines.length));
-        int lineIndex = 0;
-        for (;lineIndex < lines.length && !lines[lineIndex].equals("# organice new"); lineIndex++) {
-        }
-        lineIndex++;
-        Log.d("MyParser", "Start line: " + Integer.toString(lineIndex));
+        // read data fields line by line, until closing tag "# end organice"
+        int lineIndex = startLine;
         while(lineIndex < lines.length && !lines[lineIndex].equals("# end organice")) {
             String line = lines[lineIndex];
+            // everything before the first colon is the field name
             int colonIndex = line.indexOf(":");
             if (colonIndex < 0) {
                 throw new ParseException("", 0);
             }
-            Log.d("MyParser", "Found colon.");
             String field = line.substring(0, colonIndex);
             String s = line.substring(colonIndex+1);
-            Log.d("MyParser", "Field: " + field);
-            Log.d("MyParser", "Data: " + s);
-            if (field.equals("Title")) {
-                title = s;
-            } else if (field.equals("Start")) {
-                ds = new SimpleDateFormat("dd/MM/yyyy").parse(s);
-            } else if (field.equals("End")) {
-                de = new SimpleDateFormat("dd/MM/yyyy").parse(s);
-            } else if (field.equals("Venue")) {
-                venue = s;
-            } else if (field.equals("Note")) {
-                note = s;
-            } else {
-                throw new ParseException("", 0);
+            switch (field) {
+                case "Title":
+                    title = s;
+                    break;
+                case "Start":
+                    start = new SimpleDateFormat("dd/MM/yyyy").parse(s);
+                    break;
+                case "End":
+                    end = new SimpleDateFormat("dd/MM/yyyy").parse(s);
+                    break;
+                case "Venue":
+                    venue = s;
+                    break;
+                case "Note":
+                    note = s;
+                    break;
+                default:
+                    // invalid line, throw exception
+                    throw new ParseException("unexpected field name: \"" + field + "\"", -1);
             }
-            Log.d("MyParser", "Completed iteration.");
             lineIndex++;
         }
-        Log.d("MyParser", "Completed loop.");
-
-        EventData eventData = new EventData(title, ds, de, venue, note);
-        Log.d("MyParser", eventData.toString());
-        return new NewEventRequest(message.chatId, eventData);
+        return new EventData(title, start, end, venue, note);
     }
 
     public static void execute(CalendarHelper calendarHelper, ActionRequest request) {
         if (request instanceof NewEventRequest) {
             calendarHelper.addEvent((NewEventRequest) request);
+        } else if (request instanceof DeleteEventRequest) {
+            calendarHelper.deleteEvent((DeleteEventRequest) request);
+        } else if (request instanceof ListEventsRequest) {
+            // do nothing for now
         } else {
             Log.wtf(LOG_TAG, "unexpected request type: " + request.getClass().toString());
         }
--- app/src/main/java/edu/sutd/organice/CalendarHelper.java
+++ app/src/main/java/edu/sutd/organice/CalendarHelper.java
@@ -8,6 +8,8 @@ import android.net.Uri;
 import android.provider.CalendarContract;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TimeZone;
 
 public class CalendarHelper {
@@ -16,6 +18,22 @@ public class CalendarHelper {
 
     private Context context;
     private long calendarID;
+
+
+    private static String[] EVENT_PROJECTION = new String[]{
+            CalendarContract.Events._ID,
+            CalendarContract.Events.TITLE,
+            CalendarContract.Events.DTSTART,
+            CalendarContract.Events.DTEND,
+            CalendarContract.Events.EVENT_LOCATION,
+            CalendarContract.Events.DESCRIPTION
+    };
+    private static final int PROJECTION_EVENT_ID_INDEX = 0;
+    private static final int PROJECTION_EVENT_TITLE_INDEX = 0;
+    private static final int PROJECTION_EVENT_DTSTART_INDEX = 0;
+    private static final int PROJECTION_EVENT_DTEND_INDEX = 0;
+    private static final int PROJECTION_EVENT_LOCATION_INDEX = 0;
+    private static final int PROJECTION_EVENT_DESCRIPTION_INDEX = 0;
 
     CalendarHelper(Context c) {
         context = c;
@@ -82,6 +100,74 @@ public class CalendarHelper {
             Log.e(LOG_TAG, "failed to create new event: permission denied");
         } catch (Exception e) {
             throw e;
+        }
+    }
+
+    public void deleteEvent(DeleteEventRequest request) {
+        Log.d(LOG_TAG, "deleting event");
+
+        EventData eventData = request.eventData;
+
+        ContentResolver cr = context.getContentResolver();
+
+        Uri uri = CalendarContract.Events.CONTENT_URI;
+
+        // create selection based on available information from request
+        List<String> selectionComponents = new ArrayList<String>(5);
+        List<String> selectionArgs = new ArrayList<>(5);
+        if (eventData.title != null) {
+            selectionComponents.add("(" + CalendarContract.Events.TITLE + "= ?)");
+            selectionArgs.add(eventData.title);
+        }
+        if (eventData.dateStart != null) {
+            selectionComponents.add("(" + CalendarContract.Events.DTSTART + "= ?)");
+            selectionArgs.add(
+                    Long.toString(eventData.dateStart.getTime())
+            );
+        }
+        if (eventData.dateEnd != null) {
+            selectionComponents.add("(" + CalendarContract.Events.DTEND + "= ?)");
+            selectionArgs.add(
+                    Long.toString(eventData.dateEnd.getTime())
+            );
+        }
+        if (eventData.venue != null) {
+            selectionComponents.add("(" + CalendarContract.Events.EVENT_LOCATION + "= ?)");
+            selectionArgs.add(eventData.venue);
+        }
+        if (eventData.note != null) {
+            selectionComponents.add("(" + CalendarContract.Events.DESCRIPTION + "= ?)");
+            selectionArgs.add(eventData.note);
+        }
+
+        // get number of matching events
+        Cursor cur = cr.query(
+                uri,
+                new String[]{},
+                "(" + String.join(" AND ", selectionComponents) + ")",
+                selectionArgs.toArray(new String[0]),
+                null
+        );
+        int matches = cur.getCount();
+        cur.close();
+        switch (matches) {
+            case 1:
+                // one matching event, delete
+                cur.close();
+                cr.delete(
+                        uri,
+                        "(" + String.join(" AND ", selectionComponents) + ")",
+                        selectionArgs.toArray(new String[0])
+                );
+                Log.i(LOG_TAG, "successfully deleted event");
+                break;
+            case 0:
+                // no matching event, don't delete
+                Log.i(LOG_TAG, "no event matches delete request");
+                break;
+            default:
+                // multiple matching events, don't delete
+                Log.i(LOG_TAG, "multiple events match delete request, deletion aborted");
         }
     }
 }
